aacbafc8b9a46e66697484d97b9cdd972d1737e9
AbhishekKr-Jha <akjha4127@gmail.com>
2025-10-04 13:56:04 +0530

    assignment done

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+
+const hamburger_button  = document.getElementsByClassName('hamburger-icon')[0]
+const hamburger_menu = document.getElementsByClassName('hamburger-menu')[0]
+
+
+hamburger_button.addEventListener('click',function(){
+    hamburger_menu.classList.toggle('show-hamburger-menu')
+})
