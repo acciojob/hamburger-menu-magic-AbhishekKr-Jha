82c7e08abd033e4936fb1482c7b0a5c6bcaecb5e
AbhishekKr-Jha <akjha4127@gmail.com>
2025-10-04 14:12:54 +0530

    updated according to criteria

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
 
 
-const hamburger_button  = document.getElementsByClassName('hamburger-icon')[0]
-const hamburger_menu = document.getElementsByClassName('hamburger-menu')[0]
+const hamburger_button  = document.getElementsByClassName('ham')[0]
+const hamburger_menu = document.getElementsByClassName('nav-sub')[0]
 
 
 hamburger_button.addEventListener('click',function(){
